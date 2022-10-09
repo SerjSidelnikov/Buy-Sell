3ee78e694252fc87990621fa6856f4f239e84820
Serj Sidelnikov <serj.v.sidelnikov@gmail.com>
2022-10-09 15:40:36 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,10 @@
+build:
+	docker-compose build
+
+start:
+	docker-compose stop
+	docker-compose up
+
+stop:
+	docker-compose stop
+
---
+++ backend/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configSchemaValidation } from './config.schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ validationSchema: configSchemaValidation }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
